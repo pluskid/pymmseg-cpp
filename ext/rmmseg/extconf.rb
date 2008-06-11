fac806d6aad290dfea7dc088d13b5fbac9e8a0fd
pluskid <pluskid@gmail.com>
2008-06-11 06:29:00 -0400

    Use original compile option for Ruby instead of hard-coded one.

--- ext/rmmseg/extconf.rb
+++ ext/rmmseg/extconf.rb
@@ -1,11 +1,13 @@
 require 'mkmf'
 
-if RUBY_PLATFORM =~ /darwin/
-#  CONFIG['LDSHARED'] = 'g++ --dynamiclib -flat_namespace -undefined suppress' 
-  CONFIG['LDSHARED'] = 'g++ --dynamiclib'
-elsif RUBY_PLATFORM =~ /linux/
-  CONFIG['LDSHARED'] = 'g++ -shared'  
-end
+CONFIG['LDSHARED'] = CONFIG['LDSHARED'].sub(/^\$\(CC\)/, 'g++')
+
+# if RUBY_PLATFORM =~ /darwin/
+# #  CONFIG['LDSHARED'] = 'g++ --dynamiclib -flat_namespace -undefined suppress' 
+#   CONFIG['LDSHARED'] = 'g++ --dynamiclib'
+# elsif RUBY_PLATFORM =~ /linux/
+#   CONFIG['LDSHARED'] = 'g++ -shared'  
+# end
 
 $objs = ['algor.o', 'dict.o', 'memory.o', 'rmmseg.o']
 create_makefile('rmmseg')
