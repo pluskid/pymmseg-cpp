5ed83f22fae5864dd917fea947d67671d8360a43
pluskid <pluskid@gmail.com>
2008-08-13 11:24:18 -0400

    Solve the prob that hang up when require (OS: MacOSX-Leopard).
    Thanks nasi@github.

--- ext/rmmseg/extconf.rb
+++ ext/rmmseg/extconf.rb
@@ -9,5 +9,9 @@ CONFIG['LDSHARED'] = CONFIG['LDSHARED'].sub(/^\$\(CC\)/, 'g++')
 #   CONFIG['LDSHARED'] = 'g++ -shared'  
 # end
 
+if RUBY_PLATFORM =~ /darwin/
+  CONFIG['LDShARED'] = 'g++ -dynamiclib -single_module -flat_namespace -undefined suppress'
+end
+
 $objs = ['algor.o', 'dict.o', 'memory.o', 'rmmseg.o']
 create_makefile('rmmseg')
