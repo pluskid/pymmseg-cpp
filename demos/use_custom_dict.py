7c806d228bb857a201717f52bca536d54f9b03a4
Shuge Lee <shuge.lee@gmail.com>
2013-05-17 11:47:17 +0800

    issue #5, replace the words used in demo with more appropriate ones

--- demos/use_custom_dict.py
+++ demos/use_custom_dict.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
+"""
+generate a custom dictionary file from a plain text file
+
+    python plain2dic.py my-words.txt | sort | uniq > my-words.dic
+
+test it
+
+    python use_custom_dict.py
+"""
 import os
 import mmseg
 
@@ -7,37 +16,31 @@ import mmseg
 PWD = os.path.dirname(os.path.realpath(__file__))
 my_dict_path = os.path.join(PWD, 'my-words.dic')
 
-def get_words():
-    with open(my_dict_path) as f:
-        words = set()
-        for line in f.readlines():
-            length, word = line.split()
-            words.add(word.strip())
-        return words
 
 def test_use_custom_dict():
-    new_dicts = list(mmseg.Dictionary.dictionaries)
-    new_dicts.append(('words', my_dict_path))
-    mmseg.Dictionary.dictionaries = new_dicts
-
     mmseg.Dictionary.load_dictionaries()
 
-    words = get_words()
-    
     texts = (
-        u'人妻女教师',
-        u'齐逼小短裤',
-        u'齐b小短裤',
+        u'他来到了网易杭研大厦',
+        u'李小福是创新办主任也是云计算方面的专家',
+        u'工信处女干事每月经过下属科室都要亲口交代24口交换机等技术性器件的安装工作',
     )
 
     for text in texts:
         for token in mmseg.Algorithm(text):
-            # print '%s [%d..%d]' % (token.text, token.start, token.end)
-            assert token.text in words
-            # try:
-            #     assert token.text in words
-            # except AssertionError:
-            #     print type(token.text), token.text
+            print '%s' % (token.text),
+        print
+
+    new_dicts = list(mmseg.Dictionary.dictionaries)
+    new_dicts.append(('words', my_dict_path))
+    mmseg.Dictionary.dictionaries = new_dicts
+    mmseg.Dictionary.load_dictionaries()
+
+    print
+    for text in texts:
+        for token in mmseg.Algorithm(text):
+            print '%s' % (token.text),
+        print
     
   
 if __name__ == '__main__':
